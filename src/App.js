7490aa0b0366f5e55c94b2126e28f02b70db27c5
Ihor Nikitin <igreknet@gmail.com>
2024-05-29 11:38:03 +0300

    add project files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import Confetti from 'react-confetti';
+import Die from './components/Die/Die';
 import './App.css';
 
 function App() {
+  const [dice, setDice] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every(die => die.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+      console.log('You won!');
+    }
+  }, [dice]);
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDie());
+    }
+    return newDice;
+  }
+
+  function rollDice() {
+    if (!tenzies) {
+      setDice(oldDice =>
+        oldDice.map(die => {
+          return die.isHeld ? die : generateNewDie();
+        })
+      );
+    } else {
+      setTenzies(false);
+      setDice(allNewDice());
+    }
+  }
+  function holdDice(id) {
+    setDice(oldDice =>
+      oldDice.map(el => {
+        return el.id === id ? { ...el, isHeld: !el.isHeld } : el;
+      })
+    );
+  }
+
+  const diceElements = dice.map(el => (
+    <Die key={el.id} value={el.value} isHeld={el.isHeld} holdDice={() => holdDice(el.id)} />
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its current value between
+        rolls.
+      </p>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        {tenzies ? 'New game' : 'Roll'}
+      </button>
+    </main>
   );
 }
 
